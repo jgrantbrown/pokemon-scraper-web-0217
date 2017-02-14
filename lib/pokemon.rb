33e61da7a01a7f4caf3cd430d4c12a2faf3c19d2
Your Name <you@example.com>
2017-02-14 16:00:10 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
+require 'pry'
 class Pokemon
+ attr_reader :name,:type,:db, :id
+
+  def initialize (id = nil, name = nil, type=nil, db=nil)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+    #binding.pry
+  end
+
+  def self.save(name,type,db)
+
+    db.execute("INSERT INTO pokemon(name,type) VALUES (?,?)",name,type)
+  end
+
+  def self.find(id,db)
+   value = db.execute("SELECT name,type
+                FROM pokemon
+                WHERE id = (?)",id).flatten;
+# creation self.new = Pokemon.new
+      new_pokemon = self.new(id =id,name=value[0],type=value[1],db=db)
+# /\return
+      new_pokemon
+  end
+
 end
