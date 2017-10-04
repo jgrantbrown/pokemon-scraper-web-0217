144ad46fb355bbb6b0262d01e47792db8a8956b8
JGBrown <j.grant.brown@gmail.com>
2017-10-04 11:08:21 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,28 +1,33 @@
 require 'pry'
 class Pokemon
- attr_reader :name,:type,:db, :id
-
-  def initialize (id = nil, name = nil, type=nil, db=nil)
+ attr_reader :name,:type,:db,:id
+ attr_accessor :hp
+  def initialize (id:nil, name: nil, type: nil, db: nil, hp: nil)
+    @id = id
     @name = name
     @type = type
     @db = db
-    @id = id
+    @hp = hp
     #binding.pry
   end
 
-  def self.save(name,type,db)
-
-    db.execute("INSERT INTO pokemon(name,type) VALUES (?,?)",name,type)
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon(name, type) VALUES (?,?)",name,type)
   end
 
-  def self.find(id,db)
-   value = db.execute("SELECT name,type
-                FROM pokemon
-                WHERE id = (?)",id).flatten;
-# creation self.new = Pokemon.new
-      new_pokemon = self.new(id =id,name=value[0],type=value[1],db=db)
-# /\return
-      new_pokemon
+  def self.find(id, db)
+
+   value = db.execute("SELECT * FROM pokemon WHERE id = ?",id).flatten
+   
+  # creation self.new = Pokemon.new
+   new_pokemon=self.new(id: value[0], name: value[1], type: value[2], db: db,hp: value[3])
+
+  end
+
+  def alter_hp(new_hp, db)
+
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+
   end
 
 end
